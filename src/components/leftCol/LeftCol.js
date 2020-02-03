60e167e78348ee73c66d14c3fa7107ced5d0068e
TravelFiend <mikegrace274@gmail.com>
2020-02-02 23:36:23 -0800

    projects rendering

--- src/components/leftCol/LeftCol.js
+++ src/components/leftCol/LeftCol.js
@@ -1,23 +1,33 @@
 import React from 'react';
-import { work } from '../../resume.json';
+import { projects, work } from '../../resume.json';
 import styles from './LeftCol.css';
 
-const LeftCol = () => (
-  <section className={styles.LeftCol}>
-    <h2>Projects</h2>
-    <ul>
+const LeftCol = () => {
+  const projectElements = projects.map((project, i) => (
+    <li key={i}>
+      <h4>{project.name}</h4>
+      <h6>{project.stack}</h6>
+      <p>{project.desc}</p>
+    </li>
+  ));
 
-    </ul>
-    <h2>Work History</h2>
-    <h3>{work.company}
-      <span>{work.location}</span>
-      <span>{work.startDate}</span> - <span>{work.endDate}</span>
-    </h3>
-    <h4>{work.title}</h4>
-    <ul>
-
-    </ul>
-  </section>
-);
+  return (
+    <section className={styles.LeftCol}>
+      <h2>Projects</h2>
+      <ul>
+        {projectElements}
+      </ul>
+      <h2>Work History</h2>
+      <h3>{work.company}
+        <span>{work.location}</span>
+        <span>{work.startDate}</span> - <span>{work.endDate}</span>
+      </h3>
+      <h4>{work.title}</h4>
+      <ul>
+        {}
+      </ul>
+    </section>
+  );
+};
 
 export default LeftCol;
